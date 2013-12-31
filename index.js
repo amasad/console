1278d7355230619cb5fef7399ec55302829976c5
Amjad Masad <amjad.masad@gmail.com>
2013-12-31 01:51:15 -0500

    Fork history and fix it. Allow for navigation between lines before history.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var template = require('./template');
 var grow = require('grow');
 var domify = require('domify');
 
+var lineRe = /\r\n|\r|\n/;
+
 /**
  * @constructor
  */
@@ -20,9 +22,26 @@ function Console() {
 
 Emitter(Console.prototype);
 
+Console.prototype.$allowHistoryUp = function () {
+  var n = this.textarea.selectionStart;
+  return n <= this.value().split(lineRe)[0].length;
+};
+
+Console.prototype.$allowHistoryDown = function () {
+  var n = this.textarea.selectionStart;
+  var lines = this.value().split(lineRe);
+  if (lines.length < 2) return true;
+  lines.pop();
+  var length = lines.reduce(function (acc, text) {
+    return acc + text.length + 1;
+  }, 0);
+  return n >= length;
+};
+
 var UP = 38;
 var DOWN = 40;
 var ENTER = 13;
+
 /**
  * @param {Event} e
  */
@@ -30,14 +49,18 @@ Console.prototype.$keydown = function (e) {
   var val;
   switch (e.keyCode) {
     case UP:
-      val = this.history.prev();
-      if (val) this.value(val);
-      e.preventDefault();
+      if (this.$allowHistoryUp()) {
+        val = this.history.prev();
+        if (val) this.value(val);
+        e.preventDefault();
+      }
       break;
     case DOWN:
-      val = this.history.next();
-      if (val) this.value(val);
-      e.preventDefault();
+      if (this.$allowHistoryDown()) {
+        val = this.history.next();
+        if (val) this.value(val);
+        e.preventDefault();
+      }
       break;
     case ENTER:
       if (!e.shiftKey) {
@@ -76,6 +99,7 @@ Console.prototype.$enter = function () {
 Console.prototype.value = function (s) {
   if (typeof s === 'string') {
     this.textarea.value = s;
+    this.textarea.setAttribute('rows', s.split(lineRe).length);
     return this;
   } else {
     return this.textarea.value;
